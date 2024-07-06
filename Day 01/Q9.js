3ebeabba9c8458d45ca3cac363270b251e7aa813
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-06 23:46:08 +0530

    Q9.js

---
+++ Day 01/Q9.js
@@ -0,0 +1,6 @@
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let fname = 'Nimsara', lname = 'Gunasekara', maritalStatus = 'single', country = 'Sri Lanka', age = 23;
+console.log(fname, lname, maritalStatus, country, age)
+
+//output:- Nimsara Gunasekara single Sri Lanka 23
