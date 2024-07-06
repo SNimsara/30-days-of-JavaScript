f7b931248e26377cb38daaadef899d9e8683691b
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-06 23:52:30 +0530

    Q10.js

---
+++ Day 01/Q10.js
@@ -0,0 +1,13 @@
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 23;
+let yourAge = 30;
+
+console.log('My age is ' ,myAge, 'years old.');
+console.log('Your age is' ,yourAge, 'years old.');
+
+//output:-
+  /*
+  My age is  23 years old.
+  Your age is 30 years old.
+  */
