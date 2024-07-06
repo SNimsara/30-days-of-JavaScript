5889896f69c45c5198d8a953230478fdaadde8aa
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-06 23:25:08 +0530

    Q4.js

---
+++ Day 01/Q4.js
@@ -0,0 +1,10 @@
+// Declare variables and assign values
+let name = 'Nimsara'; //string
+let isStudent = 'true'; //boolean
+let und //undefind
+let aaa = null; //null
+
+console.log(name); // output:- Nimsara
+console.log(isStudent); // output:- true
+console.log(und); // output:- undefined
+console.log(aaa); // output:- null
