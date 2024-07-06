2d310f34a57b66e04a28bab15c6935f9d0cea214
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-06 23:41:25 +0530

    Q8.js

---
+++ Day 01/Q8.js
@@ -0,0 +1,13 @@
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let fname = 'Nimsara';
+let lname = 'Gunasekara';
+let maritalStatus = 'single';
+let country = 'SriLanka';
+let age = 23;
+
+console.log(fname); //output:- Nimsara
+console.log(lname); //output:- Gunasekara
+console.log(maritalStatus); //output:- single
+console.log(country); //output:- SriLanka
+console.log(age); //output:- 23
