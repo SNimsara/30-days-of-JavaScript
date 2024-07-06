601213308d9aac91400604cd1ae1184ef37e5e35
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-06 23:29:45 +0530

    Q5.js

---
+++ Day 01/Q5.js
@@ -0,0 +1,7 @@
+//Data types
+
+console.log (typeof 'Nimsara') // output:- string
+console.log (typeof 5) // output:- number
+console.log(typeof true) // output:- boolean
+console.log(typeof null) // output:- object type
+console.log(typeof und) // output:- undefined
