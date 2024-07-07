0d672812e3fe3e204997f18c8d23c854e8c83bfe
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-07 19:15:31 +0530

    Q7.js

---
+++ Day 02/Q7.js
@@ -0,0 +1,9 @@
+//Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+
+let string = '30 Days Of JavaScript'
+console.log(string.substring(4));
+//output:- ays Of JavaScript
+
+let string = '30 Days Of JavaScript'
+console.log(string.substring(6));
+//output:- Of JavaScript
