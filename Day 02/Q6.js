768b71db0af1ec97343cd1a3982f7e3f99d5d3dc
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-07 19:08:27 +0530

    Q6.js

---
+++ Day 02/Q6.js
@@ -0,0 +1,11 @@
+//Cut (slice) out the first word of the string using substr() or substring() method
+
+let string = '30 Days Of JavaScript'
+console.log(string.substring(0, 2)); // using substring()
+console.log(string.substr(0, 2));
+
+/*
+  output:-
+  30
+  30
+*/
