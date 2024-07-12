6fbc5bc31776f4b9fe82314d2daa11acf61cdb1b
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 13:04:07 +0530

    Q16.js

---
+++ Day 02/Q16.js
@@ -0,0 +1,6 @@
+//Use lastIndexOf to determine the position of the last occurrence of 'a' in 30 Days Of JavaScript.
+
+let sentence= '30 Days Of JavaScript'
+console.log(sentence.lastIndexOf('a'))
+
+//output:- 14
