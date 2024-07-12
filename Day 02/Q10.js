913155b3bd58eea30d0ca8c2f1ba79ad7894ce2a
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 10:58:19 +0530

    Q10.js

---
+++ Day 02/Q10.js
@@ -0,0 +1,9 @@
+//Split the string 30 Days Of JavaScript at the space using split() method
+
+let sentence = '30 Days Of JavaScript'
+
+console.log(sentence.split(''))
+
+/*
+  output:- ['3', '0', ' ', 'D', 'a', 'y', 's', ' ', 'O', 'f', ' ', 'J', 'a', 'v', 'a', 'S', 'c', 'r', 'i', 'p', 't']
+*/
