f28e58f103a9e7466d36948cdedf70877585916b
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 11:08:02 +0530

    Q12.js

---
+++ Day 02/Q12.js
@@ -0,0 +1,7 @@
+//Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
+
+let sentence = '30 Days Of JavaScript'
+console.log(sentence .replace('JavaScript', 'Python'))
+
+
+//output:- 30 Days Of Python
