83db8ad80fba1e0dd4cac9ba7b7ef254378f7515
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 12:44:30 +0530

    Q14.js

---
+++ Day 02/Q14.js
@@ -0,0 +1,6 @@
+//What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
+
+let sentence = '30 Days Of JavaScript'
+console.log(sentence.charCodeAt(11))
+
+//output:- 74
