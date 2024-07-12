8629286ab427a2db9e8789e9d08760939fba34d1
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 11:04:58 +0530

    Q11.js

---
+++ Day 02/Q11.js
@@ -0,0 +1,6 @@
+//'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+let words = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(words.split(','))
+
+//output:- ['Facebook', ' Google', ' Microsoft', ' Apple', ' IBM', ' Oracle', ' Amazon']
