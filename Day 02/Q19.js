061ba545eaf3a23c284ecca08d0bd3f99a2187dc
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 13:21:41 +0530

    Q19.js

---
+++ Day 02/Q19.js
@@ -0,0 +1,6 @@
+//Use search to find the position of the first occurrence of the word 'because' in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+let sentence = 'You cannot end a sentence with because because because is a conjunction'
+console.log(sentence.search('because'))
+
+//output:- 31
