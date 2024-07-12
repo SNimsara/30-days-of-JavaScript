7045fa6fd9f22cb571455ac110a72a2d7e45411e
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 10:49:07 +0530

    Q8.js

---
+++ Day 02/Q8.js
@@ -0,0 +1,6 @@
+//Check if the string contains a word Script using includes() method
+
+let sentence = '30 Days Of JavaScript'
+
+console.log(sentence.includes('Days'))   //true
+console.log(sentence.includes('days'))   //false
