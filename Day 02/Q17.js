3654c9165d8f42010645523096d56700db1c5a03
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 13:08:25 +0530

    Q17.js

---
+++ Day 02/Q17.js
@@ -0,0 +1,6 @@
+//Use indexOf to find the position of the first occurrence of the word 'because' in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+let sentence= 'You cannot end a sentence with because because because is a conjunction'
+console.log(sentence.indexOf('because'))
+
+//output:- 31
