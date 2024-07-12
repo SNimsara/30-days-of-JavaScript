e1c9a4449d545461743634d8f3394b62c6c28115
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 14:35:50 +0530

    Q6.js

---
+++ Day 02/Exercise: Level 2/Q6.js
@@ -0,0 +1,6 @@
+//I hope this course is not full of jargon. Check if jargon is in the sentence.
+
+let sentence = 'I hope this course is not full of jargon';
+console.log(sentence.includes('jargon'))
+
+//OUTPUT:- true
