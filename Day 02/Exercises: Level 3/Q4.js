ca052327747098a2f1d30db91c011d43c99acb73
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 20:23:41 +0530

    Q4.js

---
+++ Day 02/Exercises: Level 3/Q4.js
@@ -0,0 +1,9 @@
+//Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+
+let salary = 5000
+let bonus = 10000
+let courses = 15000
+let Aincome = (salary + bonus + courses)*12
+console.log(Aincome)
+
+//output:- 360000
