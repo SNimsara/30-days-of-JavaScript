5943871edc5fb225709c9a20aca11f0b9fe63dd7
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 14:24:58 +0530

    Q4.js

---
+++ Day 02/Exercise: Level 2/Q4.js
@@ -0,0 +1,7 @@
+//Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+
+let num1 = 9.81
+let num2 = 10
+
+console.log(num1 == num2)        //false
+console.log(Math.round(9.81))   //10
