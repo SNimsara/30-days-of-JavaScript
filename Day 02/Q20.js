6cae817b63d045ee20b6956d19219a78f8e638b5
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 13:25:37 +0530

    Q20.js

---
+++ Day 02/Q20.js
@@ -0,0 +1,6 @@
+//Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+
+let sentence = '  30 Days Of JavaScript  '
+console.log(sentence.trim(''))
+
+//output:- 30 Days Of JavaScript
