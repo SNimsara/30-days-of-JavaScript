b6063e78fb52f261764e73956dc148f482b4841b
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 13:30:34 +0530

    Q22.js

---
+++ Day 02/Q22.js
@@ -0,0 +1,6 @@
+//Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+
+let sentence = '30 Days Of JavaScrip'
+console.log(sentence.endsWith('JavaScrip'))
+
+//output:- true
