71e684cf39ab1f22dfb8c71617356c491b406736
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 13:53:02 +0530

    Q25.js

---
+++ Day 02/Q25.js
@@ -0,0 +1,6 @@
+//Use repeat() method to print 30 Days Of JavaScript 2 times
+
+let sentence = '30 Days Of JavaScript'
+console.log(sentence.repeat(2))
+
+//output:- Days Of JavaScript30 Days Of JavaScript
