66a04732f78decabca02ecda788c6634e137de15
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 13:49:54 +0530

    Q24.js

---
+++ Day 02/Q24.js
@@ -0,0 +1,6 @@
+//Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+
+let sentence = '30 Days of'
+console.log(sentence.concat(" JavaScript"))
+
+//output: Days of JavaScript
