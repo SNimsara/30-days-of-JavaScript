317225dc5e08de2d433253ec2b8f7f912bfad89b
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 10:52:09 +0530

    Q9.js

---
+++ Day 02/Q9.js
@@ -0,0 +1,6 @@
+//Split the string into an array using split() method
+
+let Name = 'nimsara'
+
+console.log(Name.split())    // ['nimsara']
+console.log(Name.split(''))   //['n', 'i', 'm', 's', 'a', 'r', 'a']
