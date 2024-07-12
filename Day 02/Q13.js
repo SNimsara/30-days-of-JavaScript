2fa3c2bb35bd7ade6598d99774b31295feb084e4
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 11:11:54 +0530

    Q13.js

---
+++ Day 02/Q13.js
@@ -0,0 +1,7 @@
+//What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
+
+let sentence = '30 Days Of JavaScript'
+console.log(sentence.charAt(15))  
+
+
+//output:- S
