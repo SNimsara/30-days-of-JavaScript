0f360fb307da8d63b0b9a5615db1b973facfc82d
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 12:58:10 +0530

    Q15.js

---
+++ Day 02/Q15.js
@@ -0,0 +1,6 @@
+//Use indexOf to determine the position of the first occurrence of 'a' in 30 Days Of JavaScript
+
+let sentence= '30 Days Of JavaScript'
+console.log(sentence.indexOf('a'))
+
+//output:- 4
