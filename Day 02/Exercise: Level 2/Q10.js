4f0273bd4aa2f0ed7ea450b72980c295e8489e85
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-12 19:48:23 +0530

    Q10.js

--- Day 02/Exercise: Level 2/Q10.js
+++ Day 02/Exercise: Level 2/Q10.js
@@ -1,9 +1,7 @@
 //Access the 'JavaScript' string characters using a random number.
 
-let str = 'JavaScript';
-let randomIndex = Math.floor(Math.random() * str.length);
-let randomCharacter = str[randomIndex];
-console.log(randomCharacter);
+let word = 'javascript';
+console.log(word[randomIndex])
 
 //output:- a
 
