d21862b66364771103c5ff4b0b6e2b4040a1ac99
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-13 16:24:37 +0530

    Q5.js

--- Day 03/Exercises: Level 1/Q5.js
+++ Day 03/Exercises: Level 1/Q5.js
@@ -9,8 +9,44 @@ Figure out the result of the following comparison expression first without using
 6) 4 === 4
 7) 4 != 4
 8) 4 !== 4
-9)  != '4'
+9) 4!= '4'
 10) 4 == '4'
 11) 4 === '4'
 12) Find the length of python and jargon and make a falsy comparison statement.
 */
+
+//1)
+console.log(4>3)      //true
+
+//2)
+console.log(4>=3)      //true
+
+//3)
+console.log(4<3)      //false
+
+//4)
+console.log(4<=3)      //false
+
+//5)
+console.log(4==4)      //true
+
+//6)
+console.log(4===4)      //true
+
+//7)
+console.log(4!=4)      //false
+
+//8)
+console.log(4!==4)      //false
+
+//9)
+console.log(4!= '4')      //false
+
+//10)
+console.log(4 == '4')      //true
+
+//11)
+console.log(4 === '4')      //false
+
+//12)
+console.log('python'.length != 'jargon'.length)      //false
