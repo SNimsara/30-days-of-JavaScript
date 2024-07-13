fb771d4f395c24de4323ea4c3c55b1796de87d1f
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-13 23:08:12 +0530

    Q2.js

---
+++ Day 03/Exercises: Level 2/Q2.js
@@ -0,0 +1,16 @@
+/*
+Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
+
+Enter side a: 5
+Enter side b: 4
+Enter side c: 3
+The perimeter of the triangle is 12
+*/
+
+let a = 5
+let b = 4
+let c = 3
+let perimeter = (a+b+c)
+console.log(perimeter)
+
+//output:- 12
