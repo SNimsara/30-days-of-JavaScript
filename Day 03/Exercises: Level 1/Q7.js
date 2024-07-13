5d68d66bcc9d12523e29e90ac462de4d47ae6036
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-13 20:53:55 +0530

    Q7.js

---
+++ Day 03/Exercises: Level 1/Q7.js
@@ -0,0 +1,13 @@
+/*
+Use the Date object to do the following activities
+
+1) What is the year today?
+2) What is the month today as a number?
+3) What is the date today?
+4) What is the day today as a number?
+5) What is the hours now?
+6) What is the minutes now?
+7) Find out the numbers of seconds elapsed from January 1, 1970 to now.
+*/
+
+//1)
