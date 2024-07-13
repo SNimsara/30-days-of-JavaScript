e6d0e950e66cfd298d04e3dcd5e4a3b12c0a8814
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-13 21:18:22 +0530

    Q7.js

--- Day 03/Exercises: Level 1/Q7.js
+++ Day 03/Exercises: Level 1/Q7.js
@@ -11,3 +11,29 @@ Use the Date object to do the following activities
 */
 
 //1)
+const now = new Date()
+console.log(now.getFullYear())     //2024
+
+//2)
+const now = new Date()
+console.log(now.getMonth())        //6  because month is july.(0-11)
+
+//3)
+const now = new Date()
+console.log(now.getDate())        //13  because the date is 13.
+
+//4)
+const now = new Date()
+console.log(now.getDay())        //6   bacause today is saturday. (0-6)
+
+//5)
+const now = new Date()
+console.log(now.getHours())      //21   because now time is 9.15 p.m.
+
+//6)
+const now = new Date()
+console.log(now.getMinutes())    //15  because now time is 9.15 p.m.
+
+//7)
+const now = new Date()
+console.log(now.getTime())       //1720885593792  this is the number of seconds passed from January 1, 1970 to July 13, 2024 09.18 p.m.
