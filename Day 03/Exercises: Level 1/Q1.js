b10eef438ad30a4f51a271ce34fe637a6c42240d
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-13 13:01:23 +0530

    Q1.js

---
+++ Day 03/Exercises: Level 1/Q1.js
@@ -0,0 +1,15 @@
+//Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+
+let firstName = 'Nimsara'
+let lastName = 'Gunasekara'
+let country = 'Sri Lanka'
+let city = 'Colombo'
+let isMarried = 'false'
+let year = 2024
+
+console.log(typeof 'Nimsara')       //string
+console.log(typeof 'Gunasekara')    //string
+console.log(typeof 'Sri Lanka')     //string
+console.log(typeof 'Colombo')       //string
+console.log(typeof false)           //boolean
+console.log(typeof 2024)            //number
