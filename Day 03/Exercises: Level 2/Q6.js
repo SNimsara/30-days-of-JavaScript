b88b1b738f3931663152df0b6deb8791422e9fea
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-13 23:51:16 +0530

    Q6.js

---
+++ Day 03/Exercises: Level 2/Q6.js
@@ -0,0 +1,10 @@
+//Slope is m = (y2-y1)/(x2-x1). Find the slope between point (2, 2) and point(6,10)
+
+let y1 = 2
+let y2 = 2
+let x1 = 10
+let x2 = 6
+let slope = ((y1-y2)/(x1-x2))
+console.log(slope)
+
+//output:- 0
