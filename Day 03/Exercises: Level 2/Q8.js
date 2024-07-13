ce38f4c719f6a63ffae7baa95dbb38916a39091d
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-14 00:07:24 +0530

    Q8.js

---
+++ Day 03/Exercises: Level 2/Q8.js
@@ -0,0 +1,7 @@
+//Calculate the value of y (y = x2 + 6x + 9). Try to use different x values and figure out at what x value y is 0.
+
+let x = prompt("Enter x value: ")
+let y =  ((x*x) + (6*x) + 9)
+console.log(y)
+
+//when x= (-3), y = 0.
