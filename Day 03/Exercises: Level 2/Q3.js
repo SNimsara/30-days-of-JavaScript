c4208ea50aaa7a8ad746d8fa6fe3dd0a7e715a3b
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-13 23:18:09 +0530

    Q3.js

---
+++ Day 03/Exercises: Level 2/Q3.js
@@ -0,0 +1,9 @@
+//Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+
+let width = 20
+let length = 10
+let area = (length*width)
+let perimeter =(2 * (length + width))
+
+console.log(area)               //200
+console.log(perimeter)          //60
