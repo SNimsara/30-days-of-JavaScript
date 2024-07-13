f88666946ad732893a9d9f19ce50461e61058410
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-14 00:30:14 +0530

    Q11.js

---
+++ Day 03/Exercises: Level 2/Q11.js
@@ -0,0 +1,20 @@
+/*
+Compare your first name length and your family name length and you should get this output.
+
+let firstName = 'Asabeneh'
+let lastName = 'Yetayeh'
+Your first name, Asabeneh is longer than your family name, Yetayeh
+*/
+
+let fname = 'Nimsara'
+let lname = 'Gunasekara'
+if (fname.length > lname.length)
+{
+    console.log(`Your first name, ${fname} is longer than your family name, ${lname}`)
+}
+else
+{
+    console.log(`Your first name, ${fname} is shoter than your family name, ${lname}`)
+}
+
+//output:- Your first name, Nimsara is shoter than your family name, Gunasekara
