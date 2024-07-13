3495265fd546790719a8815d843bcc007ead1ac8
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-13 23:39:14 +0530

    Q4.js

---
+++ Day 03/Exercises: Level 2/Q4.js
@@ -0,0 +1,9 @@
+//Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+
+const PI = 3.14
+let r = 7
+let area = (PI*r*r)
+let c = (2*PI*r)
+
+console.log(area)      //153.86
+console.log(c)         //43.96
