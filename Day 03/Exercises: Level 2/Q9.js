d9c73b0f15b2c26b811dfb203481a372ee39bf18
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-14 00:17:10 +0530

    Q9.js

---
+++ Day 03/Exercises: Level 2/Q9.js
@@ -0,0 +1,14 @@
+/*
+Writ a script that prompt a user to enter hours and rate per hour. Calculate pay of the person?
+
+Enter hours: 40
+Enter rate per hour: 28
+Your weekly earning is 1120
+*/
+
+let hours = prompt("Enter hours: ")
+let rate = prompt("Enter rate per hour: ")
+let earning = (hours*rate)
+console.log(earning)
+
+//output:- 1120
