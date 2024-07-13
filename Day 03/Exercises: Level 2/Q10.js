2328ea62c54827a7f508d54bb2172544364a2684
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-14 00:21:55 +0530

    Q10.js

---
+++ Day 03/Exercises: Level 2/Q10.js
@@ -0,0 +1,13 @@
+//If the length of your name is greater than 7 say, your name is long else say your name is short.
+
+let name = 'nimsara'
+if (name.length>=7)
+{
+    console.log("your name is long")
+}
+else
+{
+    console.log("your name is short")
+}
+
+//output:- your name is long
