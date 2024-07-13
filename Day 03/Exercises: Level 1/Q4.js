2e6169ea624d4d5324b6c75b616756d40ded213b
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-13 14:00:39 +0530

    Q4.js

---
+++ Day 03/Exercises: Level 1/Q4.js
@@ -0,0 +1,39 @@
+/*
+Boolean value is either true or false.
+
+1.Write three JavaScript statement which provide truthy value.
+2.Write three JavaScript statement which provide falsy value.
+*/
+
+// 1.
+let num1 = 10
+let num2 = '10'
+console.log(num1==num2)
+//output:- true
+
+let num1 = 10
+let num2 = 2
+console.log(num1*num2==20)
+//output:- true
+
+let num1 = 4
+let num2 = 4
+console.log(num1+num2==8)
+//output:- true
+
+
+//2.
+let w1 = 'a'
+let w2 = 'A'
+console.log(w1==w2)
+//output:- false
+
+let num1 = 5
+let num2 = 3
+console.log(num1==num2)
+//output:- false
+
+let w1 = 'b'
+let w2 = 'A'
+console.log(w1==w2)
+//output:- false
