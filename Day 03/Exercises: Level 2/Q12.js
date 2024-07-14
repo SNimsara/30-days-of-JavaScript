5ed0580b63fd854d75ac036a328a1cb4aeeeeb15
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-14 20:37:56 +0530

    Q12.js

---
+++ Day 03/Exercises: Level 2/Q12.js
@@ -0,0 +1,14 @@
+/*
+Declare two variables myAge and yourAge and assign them initial values and myAge and yourAge.
+
+let myAge = 250
+let yourAge = 25
+I am 225 years older than you.
+*/
+
+let myAge = 250
+let yourAge = 25
+let age = (myAge-yourAge)
+console.log(`I am ${age} years older than you.`)
+
+//output:- I am 225 years older than you.
