8cb5d21eefbba70c3372cfbfb19f15837768d587
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-14 22:28:26 +0530

    Q3.js

---
+++ Day 04/Exercises: Level 1/Q3.js
@@ -0,0 +1,22 @@
+/*
+If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
+
+using if else
+ternary operator.
+  let a = 4
+  let b = 3
+  4 is greater than 3
+*/
+
+let a = 4
+let b = 3
+if (a>b)
+{
+    console.log(`${a} is greater than ${b}`)
+}
+else
+{
+    console.log(`${a} is less than ${b}`)
+}
+
+//output:- 4 is greater than 3
