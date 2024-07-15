af8e1116f92180f88ec1f2772475889903e3499e
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-15 21:05:43 +0530

    Q5.js

---
+++ Day 05/Exercise: Level 1/Q5.js
@@ -0,0 +1,6 @@
+//Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+
+const mixedDataTypes = [1, 5.5, 'apple', 'Z', 0.34, 'ANT']
+console.log(mixedDataTypes.length)
+
+//output:- 6
