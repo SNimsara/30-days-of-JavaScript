00ee8e2bd2d7a0b65c39b766d64ba88837cec0aa
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-15 21:01:29 +0530

    Q4.js

---
+++ Day 05/Exercise: Level 1/Q4.js
@@ -0,0 +1,14 @@
+//Get the first item, the middle item and the last item of the array
+
+const arr = [1,2,3,4,5]
+let firstArr = arr[0]
+let lastArr = arr[4]
+let midArr = arr[2]
+
+console.log(firstArr)
+console.log(lastArr)
+console.log(midArr)
+
+//first item - 1
+//last item - 5
+//mid item - 3
