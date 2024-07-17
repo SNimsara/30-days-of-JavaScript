46fa43a8b45b8d2024326591a5f562414731db32
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-18 00:06:30 +0530

    Q4.js

--- Day 05/Exercise: Level 2/Q4.js
+++ Day 05/Exercise: Level 2/Q4.js
@@ -1,2 +1,17 @@
 //In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
 
+const countries = ['Sri Lanka' , 'Canada' , 'Australia']
+let index = countries.indexOf('Ethiopia')
+if (index === -1)
+{
+    countries.push('Ethiopia')
+    console.log(countries)
+}
+else
+{
+    console.log(Ethiopia.toUpper)
+}
+
+/* output:-
+Ethiopia doesn't exists in the array:- ['Sri Lanka', 'Canada', 'Australia', 'Ethiopia']
+Ethiopia exists in the array:- ETHIOPIA
