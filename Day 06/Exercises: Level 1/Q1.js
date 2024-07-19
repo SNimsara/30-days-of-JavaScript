e5be82e659c62520dad1c6c15609285f04242331
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-19 23:56:30 +0530

    Q1.js

---
+++ Day 06/Exercises: Level 1/Q1.js
@@ -0,0 +1,19 @@
+//Iterate 0 to 10 using for loop, do the same using while and do while loop
+
+//while loop
+let i = 0;
+while (i<11){
+    console.log(i);
+    i++;
+} 
+// output:- 0 1 2 3 4 5 6 7 8 9 10
+
+
+//do while loop
+let i = 0;
+do {
+    console.log(i)
+    i ++
+}
+    while (i<=10) 
+// output:- 0 1 2 3 4 5 6 7 8 9 10
