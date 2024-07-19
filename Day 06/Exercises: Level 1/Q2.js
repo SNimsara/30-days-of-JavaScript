2d6ce1b6e89fd4051a9ece945ccc99067288186b
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-20 00:08:13 +0530

    Q2.js

---
+++ Day 06/Exercises: Level 1/Q2.js
@@ -0,0 +1,25 @@
+//Iterate 10 to 0 using for loop, do the same using while and do while loop
+
+// for loop
+for (let i = 10; i >=0 ; i --){
+    console.log(i)
+}
+
+
+//while loop
+let i = 10;
+while (i>=0) {
+    console.log(i)
+    i --
+}
+
+
+//do while loop
+undefined
+let i = 10;
+do {
+    console.log(i)
+    i --
+} while (i>=0)
+
+//output:- 10 9 8 7 6 5 4 3 2 1 0
