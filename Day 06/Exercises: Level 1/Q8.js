fb6c520f67cd52e3c8d7b853babde050292f902d
Nimsara Gunasekara <120252248+SNimsara@users.noreply.github.com>
2024-07-20 20:19:29 +0530

    Q8.js

---
+++ Day 06/Exercises: Level 1/Q8.js
@@ -0,0 +1,10 @@
+//Use for loop to iterate from 0 to 100 and print only odd numbers
+
+for (let i = 0; i <= 100; i++) {
+    if (i%2 == 0) {
+        
+    }
+    else{
+        console.log(i)
+    }
+}
